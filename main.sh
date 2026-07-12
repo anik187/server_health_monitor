820328dc1a0264d92a65a82c3cda467a1b721c51
asif <asif.anik2000@gmail.com>
2026-07-13 04:22:35 +0600

    organized into functions

--- main.sh
+++ main.sh
@@ -2,29 +2,47 @@
 
 set -euo pipefail
 
+disk_space (){
+  df -h | awk -F " " '$1 ~ /^\/dev\/sd*/ || NR == 1 {print $0}'
+}
+
+memory_usage (){
+  local total=$(free -h | awk -F " " '/Mem:/ {print $2}')
+  local used=$(free -h | awk -F " " '/Mem:/ {print $3}')
+  local free=$(free -h | awk -F " " '/Mem:/ {print $4}')
+  echo " total: $total, used: $used, free: $free"
+}
+
+top_processes (){
+  ps aux --sort=-cpu | head -n 6 
+}
+
+logged_in_users (){
+  uptime | awk -F"," '{print $2}'
+}
+
 echo "############## memory usage of the system ##############"
 echo
 
-free -h | awk -F" " '{if(NR==1) {$1="    ";$2="total";$3="used";$4="free";$5="perc(%)" ; print $1,$2,$3,$4,$5}} /Mem:/  {$5=$2;$6=$3; gsub(/[a-zA-Z]/,"",$5); gsub(/[a-zA-Z]/,"",$6) ; if($3 ~ /[0-9]+Mi/) {$6=$6/1000}; print $1,$2,$3,$4,int($6/$5 * 100)}' 
+memory_usage
+
+# free -h | awk -F" " '{if(NR==1) {$1="    ";$2="total";$3="used";$4="free";$5="perc(%)" ; print $1,$2,$3,$4,$5}} /Mem:/  {$5=$2;$6=$3; gsub(/[a-zA-Z]/,"",$5); gsub(/[a-zA-Z]/,"",$6) ; if($3 ~ /[0-9]+Mi/) {$6=$6/1000}; print $1,$2,$3,$4,int($6/$5 * 100)}' 
 
 echo
 echo "############## disk space info for mounted filesystems ##############"
 echo
-
-df -h | awk -F " " '$1 ~ /^\/dev\/sd*/ || NR == 1 {print $0}'
-
+disk_space
 echo
 echo "############## top 5 processes as per cpu usage ##############"
 echo
 
-ps aux --sort=-cpu | head -n 6 
-
+top_processes 
 
 echo
 echo "############## current logged in users ##############"
 echo
 
-uptime | awk -F"," '{print $2}'
+logged_in_users
 
 echo
 echo "############## uptime of the machine ##############"
